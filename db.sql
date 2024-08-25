1c30f93b53b3db4f9dd718ba46f2163d32b7c9f1
MohamedTarek1993 <wwww.mtarek789@gmail.com>
2024-08-26 01:44:22 +0300

    26/8

--- db.sql
+++ db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 11, 2024 at 12:08 AM
+-- Generation Time: Aug 26, 2024 at 12:43 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.0.30
 
@@ -45,6 +45,31 @@ INSERT INTO `category` (`cat_id`, `cat_title`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `comments`
+--
+
+CREATE TABLE `comments` (
+  `comment_id` int(3) NOT NULL,
+  `comment_post_id` varchar(3) NOT NULL,
+  `comment_author` varchar(255) NOT NULL,
+  `comment_email` varchar(225) NOT NULL,
+  `comment_content` text NOT NULL,
+  `comment_status` varchar(225) NOT NULL,
+  `comment_date` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `comments`
+--
+
+INSERT INTO `comments` (`comment_id`, `comment_post_id`, `comment_author`, `comment_email`, `comment_content`, `comment_status`, `comment_date`) VALUES
+(2, '1', 'awab', 'awab@gmail.com', 'this is comment', 'Approved', '2024-08-20'),
+(7, '1', 'mohammed tarek', 'moh.tarek.dev@gmail.com', 'this is third  comment', 'Approved', '2024-08-20'),
+(11, '5', 'mohammed tarek', 'bajaxykoc@mailinator.com', 'iwish is to be good', 'Approved', '2024-08-20');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `posts`
 --
 
@@ -69,7 +94,34 @@ CREATE TABLE `posts` (
 
 INSERT INTO `posts` (`post_id`, `post_cat_id`, `post_title`, `post_author`, `post_user`, `post_date`, `post_image`, `post_content`, `post_tags`, `post_comment_count`, `post_status`, `post_views_count`) VALUES
 (1, 1, 'last news', 'awab', 'awab', '2024-08-06', 'blog1.png', 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,\r\nmolestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum\r\nnumquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium\r\noptio, eaque rerum! Provident similique accusantium nemo autem.', 'tag1 , tag2 , tag3', 0, 'draft', 0),
-(5, 3, 'Ut aliquam quis quo ', 'Sunt hic omnis simil', '', '2024-08-09', 'Marketing.png', 'Quae non sint qui du', 'Cum ullam eligendi e', 4, 'Nam cupidatat unde u', 0);
+(5, 3, 'Ut aliquam quis quo ', 'Sunt hic omnis simil', '', '2024-08-20', '12.png', '            Quae non sint qui du        \r\n        \r\n        \r\n', 'Cum ullam eligendi e', 4, 'Nam cupidatat unde u', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `user_id` int(11) NOT NULL,
+  `user_name` varchar(255) NOT NULL,
+  `user_password` varchar(32) NOT NULL,
+  `user_firstname` varchar(225) NOT NULL,
+  `user_lastname` varchar(225) NOT NULL,
+  `user_email` varchar(225) NOT NULL,
+  `user_image` text NOT NULL,
+  `user_role` varchar(225) NOT NULL,
+  `randSalt` varchar(225) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`user_id`, `user_name`, `user_password`, `user_firstname`, `user_lastname`, `user_email`, `user_image`, `user_role`, `randSalt`) VALUES
+(1, 'Awab', '123', 'Awab', 'Mohammed', 'awab@gmail.com', '', 'Admin', ''),
+(5, 'lyhaci', 'Pa$$w0rd!', 'Erich', 'Roth', 'maxuweci@mailinator.com', 'logo-png-1024x436.png', 'subscriber', ''),
+(6, 'givigisihi', 'Pa$$w0rd!', 'Mariko', 'Fields', 'didequqeh@mailinator.com', '39892.png', 'Contrbuitor', '');
 
 --
 -- Indexes for dumped tables
@@ -82,10 +134,22 @@ ALTER TABLE `category`
   ADD PRIMARY KEY (`cat_id`);
 
 --
+-- Indexes for table `comments`
+--
+ALTER TABLE `comments`
+  ADD PRIMARY KEY (`comment_id`);
+
+--
 -- Indexes for table `posts`
 --
 ALTER TABLE `posts`
   ADD PRIMARY KEY (`post_id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`user_id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -98,10 +162,22 @@ ALTER TABLE `category`
   MODIFY `cat_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
+-- AUTO_INCREMENT for table `comments`
+--
+ALTER TABLE `comments`
+  MODIFY `comment_id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
 -- AUTO_INCREMENT for table `posts`
 --
 ALTER TABLE `posts`
   MODIFY `post_id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
